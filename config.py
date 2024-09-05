36334bb86f166c8777dce4a11163c12ca3d31b78
Jaidkhanna <jaidcsc@gmail.com>
2024-09-05 19:23:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7519311991:AAFOUZNj53PR6-o8-gQkk0dQpGwNVTXyD5g")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("najmusXddos")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6366801287")
 
 
 
